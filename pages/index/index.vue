ad6c1635b3c93c8cdf54fc44aa7a8978ec43b368
Jonathan6026 <602688124@qq.com>
2021-06-11 19:37:50 +0800

    项目初始

---
+++ pages/index/index.vue
@@ -0,0 +1,45 @@
+<template>
+	<view class="content">
+		<view class="header">
+			<view class="headerLeft">
+				<text>列表</text>
+				<text>10条</text>
+			</view>
+			<view class="headerRight">
+				<view>All</view>
+				<view>ToDo</view>
+				<view>Finish</view>
+			</view>
+		</view>
+		<view class="list">
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+.header {
+	height: 40px;
+	text-align: 40px;
+	display: flex;
+}
+.headerRight {
+	display: flex;
+}
+</style>
